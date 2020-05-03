8d3a7641137c03c6617357fd1876100ceb9d19e6
Poprotskaya <daria.poprotskaya@vistajet.com>
2020-05-03 16:02:42 +0300

    Updates

---
+++ precache-manifest.20051c54bd016ec11f53a16aa24e2ead.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53127e2b9088d29637cd5a7cd13167d3",
+    "url": "/Tetris/index.html"
+  },
+  {
+    "revision": "f65f6f4fc67d0b25a445",
+    "url": "/Tetris/static/css/2.ad8fe80d.chunk.css"
+  },
+  {
+    "revision": "54992c4d6f2a18f348a3",
+    "url": "/Tetris/static/css/main.206b546b.chunk.css"
+  },
+  {
+    "revision": "f65f6f4fc67d0b25a445",
+    "url": "/Tetris/static/js/2.0f8c4947.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/Tetris/static/js/2.0f8c4947.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "54992c4d6f2a18f348a3",
+    "url": "/Tetris/static/js/main.c1a911d8.chunk.js"
+  },
+  {
+    "revision": "673fb7b51a33e48cdc7b",
+    "url": "/Tetris/static/js/runtime-main.a85269c8.js"
+  }
+]);
